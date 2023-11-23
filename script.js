300a839ca805d6b1028b281ef2be1035869c48d1
Maximilian <maximilian.kallina@gmail.com>
2023-11-23 19:44:50 +0100

    0.0.4

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ window.onload = function () {
     let j = 0;
     let lineIndex = 0;
 
+
     function typeWriterTitle() {
         if (lineIndex < titleLines.length) {
             if (i < titleLines[lineIndex].length) {
